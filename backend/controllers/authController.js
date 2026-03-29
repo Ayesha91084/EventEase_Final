76622a30d1f8660ad7b0025ad6c0a080ddef27aa
ayesha <ayeshabibi828gmail.com>
2026-03-29 17:16:38 +0500

    commit

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,11 @@ const User = require('../models/User');
 exports.registerUser = async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
+        // Check karein ke data aa raha hai
+        if(!name || !email || !password) {
+            return res.status(400).json({ success: false, message: "Please provide all details" });
+        }
+        
         const user = await User.create({ name, email, password, role });
         
         res.status(201).json({
