97d93dd61dfbcc470ccab09a5df90191d9f45a6c
ayesha <ayeshabibi828gmail.com>
2026-03-29 17:25:15 +0500

    commit

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/authController');
 
-// Ye hamara registration ka rasta hai
-// Poora URL banega: http://localhost:5000/api/v1/register
-router.post('/register', registerUser);
+router.post('/register', (req, res) => {
+    res.send("Route is working perfectly!");
+});
 
 module.exports = router;
